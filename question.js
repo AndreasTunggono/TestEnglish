2a087658d4c8cb1fa8b7664c8c03d1c5b44660be
andreastunggono <andreastunggono.at@gmail.com>
2021-12-30 16:23:43 +0700

    first commit

---
+++ question.js
@@ -0,0 +1,167 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Thomas ….. the competition in a National Olimpiade. He looks very happy.",
+    answer: "Won",
+    options: ["Jumped", "Lost", "Celebrated", "Won"],
+  },
+  {
+    numb: 2,
+    question: "My younger brother went to dentist two days ago because his ….. were in pain.",
+    answer: "Teeth",
+    options: ["Teeth", "Ears", "Hands", "Fingers"],
+  },
+  {
+    numb: 3,
+    question: "Fena : Can you lend me a hand? | Rissa : Yes, of course. What can I help you? | Fena: Please, ….. these textbooks to my office. Rissa : Yes sure, Sir.",
+    answer: "bring",
+    options: ["put", "give", "bring", "read"],
+  },
+  {
+    numb: 4,
+    question: "My sister is a nurse. She works in Anak Bunda Hospital. She ….. every patient there.",
+    answer: "Helps",
+    options: ["Sees", "Thinks", "Ignores", "Helps"],
+  },
+  {
+    numb: 5,
+    question: "John : French Fries and fried chicken are my favorite foods. | Vira: Yes, I like them too. These French Fries and fried chicken are very … .",
+    answer: "delicious",
+    options: ["salty", "sour", "delicious", "bitter"],
+  },
+  {
+    numb: 6,
+    question: "You can find …. in your kitchen.",
+    answer: "Stove",
+    options: ["Pillow", "Stove", "Television", "Sofa"],
+  },
+  {
+    numb: 7,
+    question: "My mother is a tailor. She sews and makes … .",
+    answer: "Clothes",
+    options: ["Books", "Foods", "Chairs", "Clothes"],
+  },
+  {
+    numb: 8,
+    question: "My father always reads ….. every morning in living room.",
+    answer: "Newspaper",
+    options: ["Newspaper", "Radio", "Computer", "Television"],
+  },
+  {
+    numb: 9,
+    question: "The classroom looks very … . The students should clean it soon before the teacher comes.",
+    answer: "Dirty",
+    options: ["Dirty", "Tidy", "Shiny", "Rough"],
+  },
+  {
+    numb: 10,
+    question: "Aulia : Drake, where do you want to go? | Drake : I want to ….. to Europe with my parents and siblings.",
+    answer: "move",
+    options: ["walk", "stay", "run", "move"],
+  },
+  {
+    numb: 11,
+    question: "Jeff was ill last week and he _________________ go out.",
+    answer: "Couldn't",
+    options: ["Needn't", "Can't", "Mustn't", "Couldn't"],
+  },
+  {
+    numb: 12,
+    question: "These are the photos ________________ I took on holiday.",
+    answer: "Which",
+    options: ["Which", "Who", "What", "Where"],
+  },
+  {
+    numb: 13,
+    question: "We'll stay at home if it _______________ this afternoon.",
+    answer: "Rains",
+    options: ["Raining", "Rains", "Will Rain", "Rain"],
+  },
+  {
+    numb: 14,
+    question: "He doesn't smoke now, but he __________________ a lot when he was young.",
+    answer: "Used to smoke",
+    options: ["Has smoked", "Smokes", "Used to smoke", "Was smoked"],
+  },
+  {
+    numb: 15,
+    question: "Mark plays football ___________________ anyone else I know.",
+    answer: "Better than",
+    options: ["More good than", "As better as", "Best than", "Better than"],
+  },
+  {
+    numb: 16,
+    question: "I promise I __________________ you as soon as I've finished this cleaning.",
+    answer: "will help",
+    options: ["will help", "am helping", "going to help", "have helped"],
+  },
+  {
+    numb: 17,
+    question: "This town ___________________ by lots of tourists during the summer.",
+    answer: "Is Visited",
+    options: ["Visits", "Visited", "Is visiting", "Is visited"],
+  },
+  {
+    numb: 18,
+    question: "He said that his friends ____________ to speak to him after they lost the football match.",
+    answer: "didn't want",
+    options: ["not want", "weren't", "didn't want", "aren't wanting"],
+  },
+  {
+    numb: 19,
+    question: "How about _________________ to the cinema tonight?",
+    answer: "going",
+    options: ["going", "go", "to go", "for going"],
+  },
+  {
+    numb: 20,
+    question: "Excuse me, can you ___________________ me the way to the station, please?",
+    answer: "tell",
+    options: ["give", "take", "tell", "say"],
+  },
+  {
+    numb: 21,
+    question: "I wasn't interested in the performance very much. ________________.",
+    answer: "Neither was I",
+    options: ["I didn't, too", "Neither was I", "Nor I did", "So I wasn't"],
+  },
+  {
+    numb: 22,
+    question: "Take a warm coat, _______________ you might get very cold outside.",
+    answer: "Otherwise",
+    options: ["Otherwise", "In case", "So that", "In order to"],
+  },
+  {
+    numb: 23,
+    question: "__________________ this great book and I can't wait to see how it ends.",
+    answer: "I've been reading",
+    options: ["I don't read", "I've read", "I've been reading", "I read"],
+  },
+  {
+    numb: 24,
+    question: "What I like more than anything else ___________________ at weekends.",
+    answer: "Is playing golf",
+    options: ["Playing golf", "To play golf", "Is playing golf", "Is play golf"],
+  },
+  {
+    numb: 25,
+    question: "She ________________ for her cat for two days when she finally found it in the garage.",
+    answer: "Had been looking",
+    options: ["Looked", "Had been looked", "Had been looking", "Were looking"],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
